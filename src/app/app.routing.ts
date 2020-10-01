5d43a28dd3895c154646a71f669ca7cf148f231a
nukablack <asanchezjmnz@gmail.com>
2020-10-01 12:28:49 +0200

    Creación de routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const appRoutes: Routes = [
+  { path:'', component: LoginComponent },
+  { path:'login', component: LoginComponent },
+  { path:'registro', component: RegisterComponent },
+  { path:'**', component: LoginComponent }
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes);
