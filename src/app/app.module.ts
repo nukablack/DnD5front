a5b7f670bcb54cc6336d5d1b18759e03befd31f2
nukablack <asanchezjmnz@gmail.com>
2020-10-01 13:09:21 +0200

    Registro de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { routing, appRoutingProviders } from './app.routing';
 
 // Componentes
@@ -15,6 +16,7 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     routing
   ],
   providers: [
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'register',
@@ -8,13 +9,19 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 export class RegisterComponent implements OnInit{
   public title: string;
+  public user: User;
 
   constructor(
   ){
     this.title = 'Regístrate';
+    this.user = new User(1, 'ROLE_USER', '', '', '');
   }
 
   ngOnInit(): void {
     console.log('register.component cargado correctamente');
+  }
+
+  onSubmit(){
+    console.log(this.user);
   }
 }
